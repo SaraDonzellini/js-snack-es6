8d5aa6d1eefcd9203124244423e4c040edb42de1
SaraDonzellini <saradonzie@gmail.com>
2022-12-05 11:43:47 +0100

    snack_1_studenti

---
+++ js/snack_1_studenti.js
@@ -0,0 +1,40 @@
+//? 1. Creare un oggetto che descriva una studentessa o uno studente, con le seguenti proprietà: `nome`, `cognome` e `età`.
+//? 2. Stampare a schermo attraverso un ciclo `for-in` tutte le proprietà dell'oggetto.
+//? 3. Creare un array di oggetti di studenti (ossìa oggetti con le proprietà sopra descritte) e mostriamoli.
+
+
+
+const student = {
+  name : "Giuseppina",
+  surname : "Fuffini",
+  age: 18
+}
+
+for (const chiave in student) {
+  console.log(student[chiave])
+}
+
+const students = [
+  {
+    name : "Giuseppina",
+    surname : "Fuffini",
+    age: 18
+  },
+  {
+    name : "Giangiacomo",
+    surname : "Donati",
+    age: 19
+  },
+  {
+    name : "Sarina",
+    surname : "Saretti",
+    age: 18
+  },
+  {
+    name : "Giuseppino",
+    surname : "Pasquali",
+    age: 17
+  }
+]
+
+console.log(students)
